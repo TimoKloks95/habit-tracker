3f796e24e997a469133e611ecfe511695fd979e7
TimoKloks95 <timokloks@hotmail.com>
2026-05-31 16:23:09 +0200

    Migratie voor user tabel toegevoegd.

---
+++ src/main/resources/db/migration/V1__add_user_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    username   VARCHAR(100) NOT NULL,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
