9fd40c43063fe5b56b3263894a4d66d248a62c8a
TimoKloks95 <timokloks@hotmail.com>
2026-05-31 17:20:00 +0200

    usercontroller gerefactored

--- src/main/java/com/timokloks/habit_tracker/controllers/UserController.java
+++ src/main/java/com/timokloks/habit_tracker/controllers/UserController.java
@@ -5,19 +5,17 @@ import com.timokloks.habit_tracker.dtos.UserCreatedResponse;
 import com.timokloks.habit_tracker.entities.User;
 import com.timokloks.habit_tracker.repositories.UserRepository;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
-
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @PostMapping
     public ResponseEntity<UserCreatedResponse> createUser(@Valid @RequestBody CreateUserRequest request, UriComponentsBuilder uriBuilder) {
