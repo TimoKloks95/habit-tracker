262ece8d8d5a2a73dd4f2865ae4d1dae6e0a3c79
TimoKloks95 <timokloks@hotmail.com>
2026-05-31 16:36:42 +0200

    User entiteit toegevoegd.

---
+++ src/main/java/com/timokloks/habit_tracker/entities/User.java
@@ -0,0 +1,34 @@
+package com.timokloks.habit_tracker.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
