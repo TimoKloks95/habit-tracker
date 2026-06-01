ad910c814987af1ee99150a17f5fb51ad5923cd4
TimoKloks95 <timokloks@hotmail.com>
2026-06-01 15:21:48 +0200

    Habit entiteit toegevoegd.

---
+++ src/main/java/com/timokloks/habit_tracker/entities/Frequency.java
@@ -0,0 +1,5 @@
+package com.timokloks.habit_tracker.entities;
+
+public enum Frequency {
+    DAILY, WEEKLY, MONTHLY, YEARLY;
+}
---
+++ src/main/java/com/timokloks/habit_tracker/entities/Habit.java
@@ -0,0 +1,42 @@
+package com.timokloks.habit_tracker.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Setter
+@Table(name = "habits")
+public class Habit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "frequency")
+    @Enumerated(EnumType.STRING)
+    private Frequency frequency;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
