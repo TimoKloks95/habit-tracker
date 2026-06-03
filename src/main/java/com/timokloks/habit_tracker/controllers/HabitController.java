1b6d951d2e244e1307be1d2a0fc6a877a37948f5
TimoKloks95 <timokloks@hotmail.com>
2026-06-03 15:43:06 +0200

    Service toegevoegd voor habit, en aanmaken habit functie gemaakt.

--- src/main/java/com/timokloks/habit_tracker/controllers/HabitController.java
+++ src/main/java/com/timokloks/habit_tracker/controllers/HabitController.java
@@ -2,9 +2,7 @@ package com.timokloks.habit_tracker.controllers;
 
 import com.timokloks.habit_tracker.dtos.CreateHabitRequest;
 import com.timokloks.habit_tracker.dtos.HabitResponse;
-import com.timokloks.habit_tracker.mappers.HabitMapper;
-import com.timokloks.habit_tracker.repositories.HabitRepository;
-import com.timokloks.habit_tracker.repositories.UserRepository;
+import com.timokloks.habit_tracker.services.HabitService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,26 +14,13 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequestMapping
 public class HabitController
 {
-    private final UserRepository userRepository;
-    private final HabitMapper habitMapper;
-    private final HabitRepository habitRepository;
+    private final HabitService habitService;
 
-    @PostMapping("users/{id}/habits")
+    @PostMapping("users/{userId}/habits")
     public ResponseEntity<HabitResponse> createHabit(@Valid @RequestBody CreateHabitRequest request,
-                                                     @PathVariable Long id,
+                                                     @PathVariable Long userId,
                                                      UriComponentsBuilder uriBuilder) {
-        var user = userRepository.findById(id).orElse(null);
-        if(user == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        var habit = habitMapper.toEntity(request);
-        habit.setUser(user);
-
-        habitRepository.save(habit);
-
-        var habitDto = habitMapper.toDto(habit);
-
+        var habitDto = habitService.createHabit(request,userId);
         var uri = uriBuilder.path("/habits/{id}").buildAndExpand(habitDto.getId()).toUri();
         return ResponseEntity.created(uri).body(habitDto);
     }
---
+++ src/main/java/com/timokloks/habit_tracker/services/HabitService.java
@@ -0,0 +1,34 @@
+package com.timokloks.habit_tracker.services;
+
+import com.timokloks.habit_tracker.dtos.CreateHabitRequest;
+import com.timokloks.habit_tracker.dtos.HabitResponse;
+import com.timokloks.habit_tracker.exceptions.UserNotFoundException;
+import com.timokloks.habit_tracker.mappers.HabitMapper;
+import com.timokloks.habit_tracker.repositories.HabitRepository;
+import com.timokloks.habit_tracker.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class HabitService {
+
+    private final UserRepository userRepository;
+    private final HabitMapper habitMapper;
+    private final HabitRepository habitRepository;
+
+    public HabitResponse createHabit(CreateHabitRequest request, Long userId) {
+        var user = userRepository.findById(userId).orElse(null);
+        if(user == null) {
+           throw new UserNotFoundException();
+        }
+
+        var habit = habitMapper.toEntity(request);
+        habit.setUser(user);
+
+        habitRepository.save(habit);
+
+        return habitMapper.toDto(habit);
+    }
+}
