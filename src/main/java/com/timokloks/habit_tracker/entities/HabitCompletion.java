e20f7d9785aefc9df6af3f673e76197f093195f4
TimoKloks95 <timokloks@hotmail.com>
2026-06-04 15:04:26 +0200

    Entiteit toegevoegd voor Habit completions.

---
+++ src/main/java/com/timokloks/habit_tracker/entities/HabitCompletion.java
@@ -0,0 +1,27 @@
+package com.timokloks.habit_tracker.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Setter
+@Table(name = "habit_completions")
+public class HabitCompletion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "completed_at")
+    private LocalDateTime completedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "habit_id")
+    private Habit habit;
+}
