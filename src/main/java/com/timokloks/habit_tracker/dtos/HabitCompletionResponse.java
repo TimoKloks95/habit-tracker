8514d9037eb12fb5a8862e085c74d549b767695c
TimoKloks95 <timokloks@hotmail.com>
2026-06-07 14:08:50 +0200

    HabitCompletionResponse aangepast zodat habitId niet bij elke completion wordt teruggegeven.

--- src/main/java/com/timokloks/habit_tracker/dtos/HabitCompletionResponse.java
+++ src/main/java/com/timokloks/habit_tracker/dtos/HabitCompletionResponse.java
@@ -7,6 +7,5 @@ import java.time.LocalDateTime;
 @Data
 public class HabitCompletionResponse {
     private Long id;
-    private Long habitId;
     private LocalDateTime completedAt;
 }
--- src/main/java/com/timokloks/habit_tracker/mappers/HabitCompletionMapper.java
+++ src/main/java/com/timokloks/habit_tracker/mappers/HabitCompletionMapper.java
@@ -7,6 +7,5 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface HabitCompletionMapper {
-    @Mapping(source = "habit.id", target = "habitId")
     HabitCompletionResponse toDto(HabitCompletion habitCompletion);
 }
