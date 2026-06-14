4ff1f561fa388ad41d406af23a5b20ef5a0475fc
TimoKloks95 <timokloks@hotmail.com>
2026-06-14 16:27:20 +0200

    Pom aangepast om unit test werkende te krijgen. Tests toegevoegd voor habit service.

--- src/main/java/com/timokloks/habit_tracker/services/HabitService.java
+++ src/main/java/com/timokloks/habit_tracker/services/HabitService.java
@@ -86,6 +86,4 @@ public class HabitService {
 
         return new StreakResponse(currentStreak, longestStreak);
     }
-
-
 }
---
+++ src/test/java/com/timokloks/habit_tracker/services/HabitServiceTest.java
@@ -0,0 +1,106 @@
+package com.timokloks.habit_tracker.services;
+
+import com.timokloks.habit_tracker.dtos.CreateHabitRequest;
+import com.timokloks.habit_tracker.dtos.HabitResponse;
+import com.timokloks.habit_tracker.entities.Habit;
+import com.timokloks.habit_tracker.entities.User;
+import com.timokloks.habit_tracker.mappers.HabitMapper;
+import com.timokloks.habit_tracker.repositories.HabitCompletionRepository;
+import com.timokloks.habit_tracker.repositories.HabitRepository;
+import com.timokloks.habit_tracker.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class HabitServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private HabitMapper habitMapper;
+
+    @Mock
+    private HabitRepository habitRepository;
+
+    @Mock
+    private HabitCompletionRepository habitCompletionRepository;
+
+    @Mock
+    private StreakEngine streakEngine;
+
+    @InjectMocks
+    private HabitService habitService;
+
+    // ---------------------------------------
+    // createHabit
+    // ---------------------------------------
+
+    @Test
+    void shouldCreateHabitForExistingUser() {
+        //Arrange
+        Long userId = 1L;
+        User user = new User();
+        CreateHabitRequest createHabitRequest = new CreateHabitRequest();
+        Habit habit = new Habit();
+        HabitResponse habitResponse = new HabitResponse();
+
+        when(userRepository.findById(userId))
+                .thenReturn(Optional.of(user));
+
+        when(habitMapper.toEntity(createHabitRequest))
+                .thenReturn(habit);
+
+        when(habitMapper.toDto(habit))
+                .thenReturn(habitResponse);
+
+        //Act
+        HabitResponse result = habitService.createHabit(createHabitRequest, userId);
+
+        //Assert
+        assertEquals(habit.getUser(), user);
+        verify(habitRepository).save(habit);
+        assertEquals(habitResponse, result);
+    }
+
+    @Test
+    void shouldThrowWhenCreatingHabitForUnknownUser() {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+
+    // ---------------------------------------
+    // getHabitsOfUser
+    // ---------------------------------------
+
+    @Test
+    void getHabitsOfUser() {
+    }
+
+    @Test
+    void deleteHabit() {
+    }
+
+    @Test
+    void updateHabit() {
+    }
+
+    @Test
+    void getHabit() {
+    }
+
+    @Test
+    void getStreak() {
+    }
+}
