0bcdf8ebd985c96f3772b977cef55d41c3596977
TimoKloks95 <timokloks@hotmail.com>
2026-07-01 15:18:54 +0200

    Unit test voor daily get longest streak toegevoegd.

--- src/test/java/com/timokloks/habit_tracker/services/DailyStreakStrategyTest.java
+++ src/test/java/com/timokloks/habit_tracker/services/DailyStreakStrategyTest.java
@@ -3,6 +3,7 @@ package com.timokloks.habit_tracker.services;
 import com.timokloks.habit_tracker.entities.HabitCompletion;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -11,8 +12,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiPredicate;
 
+import static net.bytebuddy.matcher.ElementMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DailyStreakStrategyTest {
@@ -66,4 +72,25 @@ class DailyStreakStrategyTest {
         assertEquals(2, result);
     }
 
+    // ---------------------------------------
+    // calculateLongestStreak
+    // ---------------------------------------
+
+    @Test
+    void longestStreakWithHappyPath() {
+        LocalDateTime firstDate = LocalDateTime.of(2026, 1, 1, 1, 1);
+        LocalDateTime secondDate = LocalDateTime.of(2026, 1, 2, 1, 2);
+        LocalDateTime thirdDate = LocalDateTime.of(2026, 1, 3, 1, 2);
+        List<HabitCompletion> completions = List.of(new HabitCompletion(firstDate), new HabitCompletion(secondDate),
+                new HabitCompletion(thirdDate));
+
+        List<LocalDate> expectedDays = List.of(firstDate.toLocalDate(), secondDate.toLocalDate(), thirdDate.toLocalDate());
+
+        when(streakCalculator.getLongestStreak(eq(expectedDays), ArgumentMatchers.any()))
+                .thenReturn(3);
+
+        int actual = dailyStreakStrategy.calculateLongestStreak(completions);
+        assertEquals(3, actual);
+        verify(streakCalculator).getLongestStreak(eq(expectedDays), ArgumentMatchers.any());
+    }
 }
