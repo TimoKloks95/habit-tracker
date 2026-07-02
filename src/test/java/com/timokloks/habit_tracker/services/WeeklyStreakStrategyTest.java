3885c1fb86e7a4f4106931b44ec9c2af1fdfde9e
TimoKloks95 <timokloks@hotmail.com>
2026-07-02 13:53:43 +0200

    Unit test voor langste wekelijkse streak toegevoegd.

--- src/test/java/com/timokloks/habit_tracker/services/WeeklyStreakStrategyTest.java
+++ src/test/java/com/timokloks/habit_tracker/services/WeeklyStreakStrategyTest.java
@@ -1,22 +1,31 @@
 package com.timokloks.habit_tracker.services;
 
+import com.timokloks.habit_tracker.dtos.WeekKey;
 import com.timokloks.habit_tracker.entities.HabitCompletion;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import java.time.LocalDateTime;
+import java.time.temporal.WeekFields;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class WeeklyStreakStrategyTest {
 
     @Mock
-    StreakCalculator calculator;
+    StreakCalculator streakCalculator;
 
     @InjectMocks
     WeeklyStreakStrategy weeklyStreakStrategy;
@@ -62,5 +71,48 @@ class WeeklyStreakStrategyTest {
 
         int result = weeklyStreakStrategy.calculateCurrentStreak(completions);
         assertEquals(2, result);
+    }
+
+    // ---------------------------------------
+    // calculateLongestStreak
+    // ---------------------------------------
+
+    @Test
+    void longestWeeklyStreakWithHappyPath() {
+        LocalDateTime now = LocalDateTime.now();
+        List<HabitCompletion> completions = List.of(new HabitCompletion(now),
+                new HabitCompletion(now.minusWeeks(1)),
+                new HabitCompletion(now.minusWeeks(2)));
+
+        WeekFields wf = WeekFields.ISO;
+
+        WeekKey firstKey =  new WeekKey(
+                now.get(wf.weekBasedYear()),
+                now.get(wf.weekOfWeekBasedYear())
+        );
+
+        WeekKey secondKey =  new WeekKey(
+                now.minusWeeks(1).get(wf.weekBasedYear()),
+                now.minusWeeks(1).get(wf.weekOfWeekBasedYear())
+        );
+
+        WeekKey thirdKey =  new WeekKey(
+                now.minusWeeks(2).get(wf.weekBasedYear()),
+                now.minusWeeks(2).get(wf.weekOfWeekBasedYear())
+        );
+
+        List<WeekKey> expectedKeys = Stream.of(firstKey, secondKey, thirdKey)
+                .sorted(Comparator
+                        .comparingInt(WeekKey::year)
+                        .thenComparingInt(WeekKey::week))
+                .toList();
+
+        when(streakCalculator.getLongestStreak(eq(expectedKeys), any()))
+                .thenReturn(3);
+
+        int result = weeklyStreakStrategy.calculateLongestStreak(completions);
+
+        assertEquals(3, result);
+        verify(streakCalculator).getLongestStreak(eq(expectedKeys), ArgumentMatchers.any());
     }
 }
